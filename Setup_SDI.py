ce9734ae2534fbc016a3058cc1b461427a0276ef
MARQyie <50098649+MARQyie@users.noreply.github.com>
2020-03-13 13:11:09 +0100

    Added SDI and SOD data setup

---
+++ Setup_SDI.py
@@ -0,0 +1,102 @@
+# Setup the Satistics on Deposit institutions dataframe
+'''
+    This script sets up the total dataframe for statistics on deposit insitutions.
+    
+    The script does the following
+    1. Reads in the SDI dataframes for each year-end from 2010 to 2017;
+       Only reads the table Assets and Liabilities
+    2. Select the relevant variables
+    3. Adds variables to the dataframe
+    4. Clean up the dataframe
+'''
+
+#------------------------------------------------------------
+# Load packages and set working directory
+#------------------------------------------------------------
+
+# Set working directory
+import os
+os.chdir(r'X:/My Documents/PhD/Materials_papers/2-Working_paper_competition')
+
+# Load packages
+import pandas as pd
+import numpy as np
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set(style = 'whitegrid', font_scale = 1.75, palette = 'Greys_d'
+
+#------------------------------------------------------------
+# Set Parameters
+#------------------------------------------------------------
+start = 2010
+end = 2017
+
+#------------------------------------------------------------
+# Load the data in one df
+#------------------------------------------------------------
+
+# Prelims
+path_sdi = r'X:/My Documents/Data/Data_SDI/{}/'
+file_sdi = r'All_Reports_{}1231_Assets and Liabilities.csv'
+vars_sdi = ['cert','fed_rssd','numemp','asset','cb']
+
+# Load data
+list_df_sdi = []
+
+for year in range(start,end + 1):
+    # Load the dataframe in a temporary frame
+    df_sdi_load = pd.read_csv(path_sdi.format(year) + file_sdi.format(year), usecols = vars_sdi)
+    
+    # Add a date var
+    df_sdi_load['date'] = year
+    
+    # Append to list
+    list_df_sdi.append(df_sdi_load)
+
+# Make a dataframe from the list
+df_sdi = pd.concat(list_df_sdi)
+
+#------------------------------------------------------------
+# Add variables to the dataframe
+#------------------------------------------------------------
+
+df_sdi['ln_ta'] = np.log(df_sdi.asset)
+df_sdi['ln_emp'] = np.log(1 + df_sdi.numemp)
+
+#------------------------------------------------------------
+# Clean the dataframe
+#------------------------------------------------------------
+
+# Drop all missings in cert, numemp, asset, cb
+df_sdi.dropna(subset = ['cert', 'numemp', 'asset','cb'], inplace = True)
+
+# Restrict assets and numemp to be positive
+df_sdi = df_sdi[(df_sdi.asset >= 0.0) | (df_sdi.numemp >= 0.0)]
+
+# Check for outliers in assets and numemp
+## Box plots
+dict_var_names = {'ln_ta':'Log Total Assets',\
+                  'ln_emp':'Log Total Employees'}
+vars_needed = ['ln_ta', 'ln_emp']
+
+for var in vars_needed:
+    fig, ax = plt.subplots(figsize=(12, 8))
+    plt.title('{}'.format(dict_var_names[var]))
+    
+    data = df_sdi[var]
+    ax.boxplot(data)
+    
+    plt.xticks([1], ['Full Sample SDI'])
+    
+    fig.savefig('Figures\Box_plots\Box_SDI_{}.png'.format(var)) # MAKE FOLDER!!!
+    plt.clf()
+
+## remove outliers
+#TODO
+
+#------------------------------------------------------------
+# Save the dataframe
+#------------------------------------------------------------
+    
+df_sdi.to_csv('Data/df_sdi_wp2.csv')
---
+++ Setup_SOD.py
@@ -0,0 +1,108 @@
+# Setup the Summary of Deposits dataframe
+'''
+    This script sets up the total dataframe for statistics on deposit insitutions.
+    
+    The script does the following
+    1. Reads in the SOD dataframes for each year-end from 2010 to 2017
+    2. Select the relevant variables
+    3. Clean up the dataframe
+'''
+
+#------------------------------------------------------------
+# Load packages and set working directory
+#------------------------------------------------------------
+
+# Set working directory
+import os
+os.chdir(r'X:/My Documents/PhD/Materials_papers/2-Working_paper_competition')
+
+# Load packages
+import pandas as pd
+import numpy as np
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set(style = 'whitegrid', font_scale = 1.75, palette = 'Greys_d'
+
+#------------------------------------------------------------
+# Set Parameters
+#------------------------------------------------------------
+start = 2010
+end = 2017
+
+#------------------------------------------------------------
+# Load the data in one df
+#------------------------------------------------------------
+
+# Prelims
+path_sod = r'X:/My Documents/Data/Data_SOD/'
+file_sod = r'ALL_{}.csv'
+vars_sod = ['CERT','MSABR','RSSDID','STCNTYBR','SIMS_LATITUDE','SIMS_LONGITUDE',\
+            'STALPBR','BRSERTYP','UNINUMBR','DEPSUMBR']
+dtypes_col_sod = {'MSABR':'str','STCNTYBR':'str'}
+
+# Load data
+list_df_sod = []
+
+for year in range(start,end + 1):
+    # Load the dataframe in a temporary frame
+    df_sod_load = pd.read_csv(path_sod + file_sod.format(year),\
+                              usecols = vars_sod, dtype = dtypes_col_sod)
+    # Correct STCNTYBR
+    df_sod_load['STCNTYBR'] = df_sod_load.STCNTYBR.str.zfill(5)
+    
+    # Fix DEPSUMBR (reads in as object, but should be an int)
+    df_sod_load['DEPSUMBR'] = df_sod_load.DEPSUMBR.str.replace(',','').astype(int)
+    
+    # Force lowercase column names
+    df_sod_load.columns = map(str.lower, df_sod_load.columns)
+    
+    # Rename columns
+    df_sod_load.rename(columns = {'stcntybr':'fips'}, inplace = True)
+    
+    # Add a date var
+    df_sod_load['date'] = year
+    
+    # Append to list
+    list_df_sod.append(df_sod_load)
+
+# Make a dataframe from the list
+df_sod = pd.concat(list_df_sod)
+
+#------------------------------------------------------------
+# Clean the dataframe
+#------------------------------------------------------------
+
+# Drop all missings in cert, numemp, asset, cb
+df_sdi.dropna(subset = ['cert', 'fips', 'sims_latitude', 'sims_longitude', 'depsumbr'], inplace = True)
+
+# Restrict deposits
+df_sod = df_sod[(df_sod.depsumbr >= 0.0)]
+
+# Check for outliers in assets and numemp
+## Box plots
+dict_var_names = {'sims_latitude':'Log Total Assets',\
+                  'sims_longitude':'Log Total Employees',\
+                  'depsumbr':'Branch Deposits'}
+vars_needed = ['sims_latitude', 'sims_longitude', 'depsumbr']
+
+for var in vars_needed:
+    fig, ax = plt.subplots(figsize=(12, 8))
+    plt.title('{}'.format(dict_var_names[var]))
+    
+    data = df_sdi[var]
+    ax.boxplot(data)
+    
+    plt.xticks([1], ['Full Sample SOD'])
+    
+    fig.savefig('Figures\Box_plots\Box_SOD_{}.png'.format(var)) # MAKE FOLDER!!!
+    plt.clf()
+
+## remove outliers
+#TODO
+
+#------------------------------------------------------------
+# Save the dataframe
+#------------------------------------------------------------
+    
+df_sdi.to_csv('Data/df_sod_wp2.csv')
