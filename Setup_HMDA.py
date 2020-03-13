e30ef9517389a18ff626854e921084404ba5a0ab
MARQyie <50098649+MARQyie@users.noreply.github.com>
2020-03-13 13:41:16 +0100

    Setup SOD and HMDA

--- Setup_HMDA.py
+++ Setup_HMDA.py
@@ -1,11 +1,16 @@
-# Setup the Summary of Deposits dataframe
+# Setup the HMDA dataframe
 '''
-    This script sets up the total dataframe for SODs.
+    This script sets up the total dataframe for HMDA. This script also reads
+    the HMDA lender file (avery file)
     
     The script does the following
-    1. Reads in the SOD dataframes for each year-end from 2010 to 2017
+    1. Reads in the HMDA dataframes for each year-end from 2010 to 2017
     2. Select the relevant variables
     3. Clean up the dataframe
+    
+    Concatenating all HMDA files might lead to an overload of memory usage. 
+    Strategy: load all HMDA files separately, clean them and save them as 
+    zipped csv (saves space).
 '''
 
 #------------------------------------------------------------
@@ -35,30 +40,30 @@ end = 2017
 #------------------------------------------------------------
 
 # Prelims
-path_sod = r'X:/My Documents/Data/Data_SOD/'
-file_sod = r'ALL_{}.csv'
-vars_sod = ['CERT','MSABR','RSSDID','STCNTYBR','SIMS_LATITUDE','SIMS_LONGITUDE',\
-            'STALPBR','BRSERTYP','UNINUMBR','DEPSUMBR']
-dtypes_col_sod = {'MSABR':'str','STCNTYBR':'str'}
+## Lender File
+path_lf = r'X:/My Documents/Data/Data_HMDA_lenderfile/'
+file_lf = r'hmdpanel17.dta'
+vars_lf = ['hmprid'] + ['CERT{}'.format(str(year)[2:4]) for year in range(start, end +1)] \
+          + ['ENTITY{}'.format(str(year)[2:4]) for year in range(start, end +1)] \
+          + ['RSSD{}'.format(str(year)[2:4]) for year in range(start, end +1)]
+
+## HMDA
+path_hmda = r'X:/My Documents/Data/Data_HMDA/LAR/'
+file_hmda = r'hmda_{}_nationwide_originated-records_codes.zip'
+dtypes_col_hmda = {'state_code':'str', 'county_code':'str','msamd':'str',\
+                   'census_tract_number':'str'}
+na_values = ['NA   ', 'NA ', '...',' ','NA  ','NA      ','NA     ','NA    ','NA']
 
 # Load data
-list_df_sod = []
+## Lender file
+df_lf = pd.read_stata(path_lf + file_lf, columns = vars_lf)
 
+# HMDA
 for year in range(start,end + 1):
-    # Load the dataframe in a temporary frame
-    df_sod_load = pd.read_csv(path_sod + file_sod.format(year),\
-                              usecols = vars_sod, dtype = dtypes_col_sod)
-    # Correct STCNTYBR
-    df_sod_load['STCNTYBR'] = df_sod_load.STCNTYBR.str.zfill(5)
-    
-    # Fix DEPSUMBR (reads in as object, but should be an int)
-    df_sod_load['DEPSUMBR'] = df_sod_load.DEPSUMBR.str.replace(',','').astype(int)
-    
-    # Force lowercase column names
-    df_sod_load.columns = map(str.lower, df_sod_load.columns)
-    
-    # Rename columns
-    df_sod_load.rename(columns = {'stcntybr':'fips'}, inplace = True)
+     #Load the dataframe in a temporary frame
+     df_chunk = pd.read_csv(path_hmda + file_hmda.format(year)), index_col = 0, chunksize = 1e6, \
+                  na_values = na_values, dtype = dtypes_col_hmda)
+
     
     # Add a date var
     df_sod_load['date'] = year
