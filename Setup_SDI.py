a72a5eeed1ee6cc14d198b4ceb80bf8fca1dc760
MARQyie <50098649+MARQyie@users.noreply.github.com>
2020-09-28 11:10:03 +0200

    Changes SDI 20200928

--- Setup_SDI.py
+++ Setup_SDI.py
@@ -1,7 +1,7 @@
 # Setup the Satistics on Deposit institutions dataframe
 '''
     This script sets up the total dataframe for statistics on deposit insitutions.
-    
+
     The script does the following
     1. Reads in the SDI dataframes for each year-end from 2010 to 2017;
        Only reads the table Assets and Liabilities
@@ -48,10 +48,10 @@ list_df_sdi = []
 for year in range(start,end + 1):
     # Load the dataframe in a temporary frame
     df_sdi_load = pd.read_csv(path_sdi.format(year) + file_sdi.format(year), usecols = vars_sdi)
-    
+
     # Add a date var
     df_sdi_load['date'] = year
-    
+
     # Append to list
     list_df_sdi.append(df_sdi_load)
 
@@ -84,18 +84,18 @@ vars_needed = ['ln_ta', 'ln_emp']
 for var in vars_needed:
     fig, ax = plt.subplots(figsize=(12, 8))
     plt.title('{}'.format(dict_var_names[var]))
-    
+
     data = df_sdi[var]
     ax.boxplot(data)
-    
+
     plt.xticks([1], ['Full Sample SDI'])
-    
-    fig.savefig('Figures\Box_plots\Box_SDI_{}.png'.format(var)) 
+
+    fig.savefig('Figures\Box_plots\Box_SDI_{}.png'.format(var))
     plt.clf()
 
 ## remove outliers
 # NOTE: No outliers to remove
-    
+
 #------------------------------------------------------------
 # Save the dataframe
 #------------------------------------------------------------
